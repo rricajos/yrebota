b2a1417d516e9d7f3bec8c2a1703bcd549e505e2
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-09-28 18:17:54 +0200

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,13 +1,13 @@
 <?php
 // Definir las contraseñas por defecto para cada usuario
 $password1 = password_hash("password123", PASSWORD_DEFAULT);
-$password2 = password_hash("web2024", PASSWORD_DEFAULT);
+$password2 = password_hash("sandia_humana", PASSWORD_DEFAULT);
 $password3 = password_hash("daw_project", PASSWORD_DEFAULT);
 $password4 = password_hash("grupo4clave", PASSWORD_DEFAULT);
 
 // Guardar correos y contraseñas hasheadas en el archivo correos.txt
 file_put_contents('data/correos.txt', "correo1@example.com;$password1\n", FILE_APPEND);
-file_put_contents('data/correos.txt', "correo2@example.com;$password2\n", FILE_APPEND);
+file_put_contents('data/correos.txt', "barbarap.piris@gmail.com;$password2\n", FILE_APPEND);
 file_put_contents('data/correos.txt', "correo3@example.com;$password3\n", FILE_APPEND);
 file_put_contents('data/correos.txt', "correo4@example.com;$password4\n", FILE_APPEND);
 
