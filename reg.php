5bc7152dfc85604cc278e8c0e33cfaf265c0e63e
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-09-28 16:53:44 +0200

    Update reg.php

--- reg.php
+++ reg.php
@@ -1,14 +1,15 @@
 <?php
-// Este script solo se usa una vez para generar las contraseñas hasheadas
-$password1 = password_hash("contraseña1", algo: "patata");
-$password2 = password_hash("contraseña2", "sandia");
-$password3 = password_hash("contraseña3", "cereza");
-$password4 = password_hash("contraseña4", "naranja");
+// Definir las contraseñas por defecto para cada usuario
+$password1 = password_hash("patata", PASSWORD_DEFAULT);
+$password2 = password_hash("sandia", PASSWORD_DEFAULT);
+$password3 = password_hash("cereza", PASSWORD_DEFAULT);
+$password4 = password_hash("papaya", PASSWORD_DEFAULT);
 
-file_put_contents('data/correos.txt', "correo1@example.com;$password1\n", FILE_APPEND);
+// Guardar correos y contraseñas hasheadas en el archivo correos.txt
+file_put_contents('data/correos.txt', "ergoegos@gmail.com;$password1\n", FILE_APPEND);
 file_put_contents('data/correos.txt', "correo2@example.com;$password2\n", FILE_APPEND);
 file_put_contents('data/correos.txt', "correo3@example.com;$password3\n", FILE_APPEND);
 file_put_contents('data/correos.txt', "correo4@example.com;$password4\n", FILE_APPEND);
 
-echo "Contraseñas generadas y guardadas en correos.txt";
+echo "Contraseñas predeterminadas generadas y guardadas en correos.txt";
 ?>
